700633d3c534b1efc5693f1597f5e8a002c933fc
Eric Charnesky <eric.charnesky@careevolution.com>
2021-01-12 11:34:54 -0500

    printing hello world!

--- HelloWorld/src/helloworld/HelloWorld.java
+++ HelloWorld/src/helloworld/HelloWorld.java
@@ -15,7 +15,8 @@ public class HelloWorld {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello world!");
+        System.out.println("Hello faster!");
     }
     
 }
