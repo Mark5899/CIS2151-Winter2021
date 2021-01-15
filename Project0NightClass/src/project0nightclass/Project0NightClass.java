ac324c8492781e05a6c08c29b5f450e40b59e94b
Eric Charnesky <eric.charnesky@careevolution.com>
2021-01-14 23:12:05 -0500

    citation!

--- Project0NightClass/src/project0nightclass/Project0NightClass.java
+++ Project0NightClass/src/project0nightclass/Project0NightClass.java
@@ -6,7 +6,7 @@ public class Project0NightClass {
 
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        
+        // copied from https://github.com/EricCharnesky/CIS2151-Winter2021/blob/main/Project0NightClass/src/project0nightclass/Project0NightClass.java
         System.out.println("Hello generic humanoid, I am the mighty computer, what is your name?");
         String name = keyboard.nextLine();
         System.out.println("Hello " + name + ", nice to meet you, how are you?");
